a0869ff2b920ccd33e91f7e451a24e838395da81
Fataltester229 <juanm46891@gmail.com>
2025-10-22 09:50:56 -0500

    estructura del proxy

--- collatz/src/main/java/edu/eci/arep/collatz/CollatzController.java
+++ collatz/src/main/java/edu/eci/arep/collatz/CollatzController.java
@@ -4,8 +4,11 @@
  */
 package edu.eci.arep.collatz;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,5 +18,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CollatzController {
     
+    private static LinkedHashMap collatzsecuence = new LinkedHashMap<>();
+    
+    @GetMapping("/collatz")
+    public ResponseEntity<String> collatzRespon(@RequestParam String value){
+        LinkedHashMap ans = collatz(value);
+        
+        return ResposeEntity.ok(200).body(ans);
+    }
+    
+    private static LinkedHashMap collatz(int value){
+        if(value == 1){
+            return collatzsecuence;
+        }else if(value%2 == 0){
+            value = value/2;
+            collatzsecuence.put(value, value);
+            return collatz(value);
+        }else if(value%2 == 1){
+            value = (3*value)+1;
+            collatzsecuence.put(value, value);
+            return collatz((3*value)+1);
+        }
+        return null;
+    }
     
 }
--- proxy/src/main/java/edu/eci/arep/proxy/ProxyController.java
+++ proxy/src/main/java/edu/eci/arep/proxy/ProxyController.java
@@ -3,11 +3,67 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package edu.eci.arep.proxy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  *
  * @author juan.mmendez
  */
+@RestController
 public class ProxyController {
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static String GET_URL;
+    int selection;
+    @GetMapping("/collatz")
+    public void proxyRespon(@RequestParam String value) throws ProtocolException, IOException{
+        GET_URL = ""+"?value="+value;
+        HttpConnection(value);
+    }
+
+    
+    
+    private void HttpConnection(String value) throws MalformedURLException, ProtocolException, IOException{
+        
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+        } else {
+            GET_URL = ""+"?value="+value;
+            HttpConnection(value);
+        }
+        System.out.println("GET DONE");
+    
+    }
     
 }
